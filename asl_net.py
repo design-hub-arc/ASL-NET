81607c4842aab02b2eaaa3d39516162df7f7642b
nickpavini <nickpavini@gmail.com>
2021-03-09 13:44:16 -0500

    Add import functionality to testing so we can compile to a single executable

--- asl_net.py
+++ asl_net.py
@@ -94,6 +94,76 @@ def TestNetwork(model, dataset):
     print("Correct = " + str(num_correct) + ", Total Predictions = " + str(total_predictions) + ", Validation Accuracy = " + str(float(num_correct)/float(total_predictions)))
 
 
+# use this to test a fully trained model
+def TestNetworkNotTraining(dataset_directory, batch_size, model_path, output_pdf_path):
+        model = tf.keras.models.load_model(model_path) # load trained model
+        image_resizer.Resize(dataset_directory, dataset_directory, 64, 64) # resize images to test them
+        images = os.listdir(dataset_directory) # list of all images
+        numOfBatches = len(images) / batch_size if len(images) % batch_size == 0 else len(images) / batch_size + 1
+        
+        
+        output_pdf = fitz.open() # pdf handle
+        cur_page = output_pdf.newPage() # insert first page
+        
+        # predefine rectangles for photos per page (15 per page).. later on maybe locations will want to be dynamic but for now we will keep reporting assumed to be >
+        image_rectangles = [fitz.Rect(67, 52, 131, 116), fitz.Rect(265, 52, 329, 116), fitz.Rect(463, 52.2, 527, 116),
+                      fitz.Rect(67, 220, 131, 284), fitz.Rect(265, 220, 329, 284), fitz.Rect(463, 220, 527, 284),
+                      fitz.Rect(67, 388, 131, 452), fitz.Rect(265, 388, 329, 452), fitz.Rect(463, 388, 527, 452),
+                      fitz.Rect(67, 556, 131, 620), fitz.Rect(265, 556, 329, 620), fitz.Rect(463, 556, 527, 620),
+                      fitz.Rect(67, 724, 131, 788), fitz.Rect(265, 724, 329, 788), fitz.Rect(463, 724, 527, 788)]
+        
+        # descriptions on page will be relative to the image placements (also 15 per page)
+        results_rectangles = [fitz.Rect(image_rectangles[0].x0-32, image_rectangles[0].y1+15, image_rectangles[0].x1+32, image_rectangles[0].y1+45),
+                              fitz.Rect(image_rectangles[1].x0-32, image_rectangles[1].y1+15, image_rectangles[1].x1+32, image_rectangles[1].y1+45),
+                              fitz.Rect(image_rectangles[2].x0-32, image_rectangles[2].y1+15, image_rectangles[2].x1+32, image_rectangles[2].y1+45),
+                              fitz.Rect(image_rectangles[3].x0-32, image_rectangles[3].y1+15, image_rectangles[3].x1+32, image_rectangles[3].y1+45),
+                              fitz.Rect(image_rectangles[4].x0-32, image_rectangles[4].y1+15, image_rectangles[4].x1+32, image_rectangles[4].y1+45),
+                              fitz.Rect(image_rectangles[5].x0-32, image_rectangles[5].y1+15, image_rectangles[5].x1+32, image_rectangles[5].y1+45),
+                              fitz.Rect(image_rectangles[6].x0-32, image_rectangles[6].y1+15, image_rectangles[6].x1+32, image_rectangles[6].y1+45),
+                              fitz.Rect(image_rectangles[7].x0-32, image_rectangles[7].y1+15, image_rectangles[7].x1+32, image_rectangles[7].y1+45),
+                              fitz.Rect(image_rectangles[8].x0-32, image_rectangles[8].y1+15, image_rectangles[8].x1+32, image_rectangles[8].y1+45),
+                              fitz.Rect(image_rectangles[9].x0-32, image_rectangles[9].y1+15, image_rectangles[9].x1+32, image_rectangles[9].y1+45),
+                              fitz.Rect(image_rectangles[10].x0-32, image_rectangles[10].y1+15, image_rectangles[10].x1+32, image_rectangles[10].y1+45),
+                              fitz.Rect(image_rectangles[11].x0-32, image_rectangles[11].y1+15, image_rectangles[11].x1+32, image_rectangles[11].y1+45),
+                              fitz.Rect(image_rectangles[12].x0-32, image_rectangles[12].y1+15, image_rectangles[12].x1+32, image_rectangles[12].y1+45),
+                              fitz.Rect(image_rectangles[13].x0-32, image_rectangles[13].y1+15, image_rectangles[13].x1+32, image_rectangles[13].y1+45),
+                              fitz.Rect(image_rectangles[14].x0-32, image_rectangles[14].y1+15, image_rectangles[14].x1+32, image_rectangles[14].y1+45)]
+
+
+        num_pics_on_page = 0 # current number of photos on the page
+        
+        # go through each batch
+        for j in range(int(numOfBatches)):
+            if j == int(numOfBatches)-1:
+                batch_images = np.zeros((len(images) - j*batch_size, 64, 64, 3), dtype=np.float32) # 200x200 RGB images
+                for i in range(len(images) - j*batch_size):
+                    batch_images[i] = mpimg.imread(os.path.join(dataset_directory,images[j*batch_size + i])) # get the images 1 by 1
+            else:
+                batch_images = np.zeros((batch_size, 64, 64, 3), dtype=np.float32) # 200x200 RGB images
+                for i in range(batch_size):
+                    batch_images[i] = mpimg.imread(os.path.join(dataset_directory,images[j*batch_size + i])) # get the images 1 by 1
+            
+            predictions = model.predict_on_batch(batch_images) # get predictions
+            
+            # print the results
+            for i in range(len(predictions)):
+                if num_pics_on_page < len(image_rectangles): # add to current page if there is space
+                    cur_page.insertImage(image_rectangles[num_pics_on_page], filename=os.path.join(dataset_directory,images[j*batch_size + i]))
+                    cur_page.insertTextbox(results_rectangles[num_pics_on_page], 'Image: ' + images[j*batch_size + i] + '\n' + 'Prediction: ' + chr(65 + np.argmax(predictions[i])), align=1)
+                else:
+                    cur_page = output_pdf.newPage() # create a new page
+                    num_pics_on_page = 0 # fresh page
+                    cur_page.insertImage(image_rectangles[num_pics_on_page], filename=os.path.join(dataset_directory,images[j*batch_size + i])) # add to the new page
+                    cur_page.insertTextbox(results_rectangles[num_pics_on_page], 'Image: ' + images[j*batch_size + i] + '\n' + 'Prediction: ' + chr(65 + np.argmax(predictions[i])), align=1)
+                    
+                print('Image Name: ' + images[j*batch_size + i], end=' ') # print statements for quick visual that its working
+                print('Prediction: ' + chr(65 + np.argmax(predictions[i])))
+                
+                num_pics_on_page += 1 # increment images on page
+        
+        output_pdf.save(output_pdf_path) # save pdf one dir back
+
+
 if __name__ == "__main__":
 
     # get directory and batch size
